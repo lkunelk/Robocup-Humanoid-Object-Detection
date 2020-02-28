98e9b8af4c325b329fc5ebd7da8c78611831c690
Nam <lkunelk@gmail.com>
2020-02-28 15:57:11 -0500

    set up working model file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import torch
+import numpy as np
 import model
+
+m = model.CNN()
+x = np.ones((1, 1, 3, 3))
+x = torch.Tensor(x)
+y = m.forward(x)
+print(y)
--- model.py
+++ model.py
@@ -1,1 +1,15 @@
-print('module')
+import torch
+import torch.nn as nn
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 1, (2, 2))
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+        return x
