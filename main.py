7c1e8939efb5611d8d2546def5edbafb01134ecd
Nam Nguyen <lkunelk@gmail.com>
2020-03-26 12:43:35 -0400

    succesfully find bounding rectangle for constructed ellipse in image

--- main.py
+++ main.py
@@ -29,26 +29,32 @@ def train_model():
     torch.save(model.state_dict(), 'outputs/model')
 
 def test_bounding_box():
-    train, valid, test = initialize_loader(10)
+    # train, valid, test = initialize_loader(10)
+    #
+    # model = CNN()
+    # model.load_state_dict(torch.load('outputs/model'))
+    # model.eval()
+    #
+    # for batch, mask in train:
+    #     _, output = model(batch.float())
+    #     display_image(pred=output[0])
+    #     break
 
-    model = CNN()
-    model.load_state_dict(torch.load('outputs/model'))
-    model.eval()
-
-    for batch, mask, bbx in train:
-        output, _ = model(batch.float())
-        display_image(pred=output[0])
-        break
-
-    img = np.zeros((152, 200, 1))
+    img = np.zeros((150, 200, 1))
     center = (75, 40)
     size = (20, 30)
-    img = cv2.ellipse(img, center, size, 0, 0, 360, (1), -1)
+    img = cv2.ellipse(img, center, size, 0, 0, 360, (255), -1)
     cv2.imshow('hi?', img)
     cv2.waitKey(0)
-    img = np.rollaxis(img, 2)
-    bbx = find_bounding_boxes(torch.Tensor(img))
-    print(bbx)
+    img = img.astype(np.uint8)
+    print(img.shape, img.dtype)
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # print(contours)
+    x, y, w, h = cv2.boundingRect(contours[0])
+
+    # img = np.rollaxis(img, 2)
+    # bbx = find_bounding_boxes(torch.Tensor(img))
+    print(x, y, w, h)
 
 if __name__ == '__main__':
-    train_model()
+    test_bounding_box()
